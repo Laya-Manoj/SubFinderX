b09208bcae2de05dfaf71c08db402094c89f09cb
Laya Manoj <laya.m@prudentconsulting.com>
2026-06-08 13:59:29 +0530

    Update backend API URL to EC2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -56,7 +56,7 @@ function App() {
         formData.append("wordlist_file", wordlistFile);
       }
 
-      const response = await fetch("http://127.0.0.1:5000/scan", {
+      const response = await fetch("http://13.233.199.106:5000/scan", {
         method: "POST",
         body: formData,
         mode: "cors",
